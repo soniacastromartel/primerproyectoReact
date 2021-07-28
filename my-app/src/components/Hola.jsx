1eaa1b1af84556878e129996d47c9166798c9afc
Sonia Castro <soniacastromartel@gmail.com>
2021-07-28 11:56:55 +0100

    1.2 Jsx & Fragments

--- my-app/src/components/Hola.jsx
+++ my-app/src/components/Hola.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-function Hola (){
-    return(
-        <div>
-            <h2>Primer Componente</h2>
-        </div>
-    )
+function Hola() {
+  let componente = "en React"; //Jsx intercalación de Javascript con elementos HTML
+  return (
+    <Fragment>
+      <h2>Primer Componente {componente}</h2>
+      <p>Otro texto</p>
+    </Fragment>
+  );
 }
 export default Hola;
