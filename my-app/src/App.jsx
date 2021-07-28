01dd460100c11cd257478b0e0e9e2b9e93b3829b
Sonia Castro <soniacastromartel@gmail.com>
2021-07-28 11:39:30 +0100

    1.1 primer componente

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Hola from './components/Hola';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Titulo de Mi Primer Proyecto</h1>
+     <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Hola.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Hola (){
+    return(
+        <div>
+            <h2>Primer Componente</h2>
+        </div>
+    )
+}
+export default Hola;
