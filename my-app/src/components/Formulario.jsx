3fb71134fde33eb9afdb25645325ec7a09620a5c
Sonia Castro <soniacastromartel@gmail.com>
2021-07-30 10:33:14 +0100

    1.8  onChange events

--- my-app/src/components/Formulario.jsx
+++ my-app/src/components/Formulario.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Formulario = () => {
-    return (
-        <div className='container'>
-            <form className='form-group'>
-                <input placeholder= 'Introduzca el nombre' className='form-control mb-3' type="text" />
-                <input placeholder= 'Introduzca la edad' className='form-control  mb-3' type="text" />
-                <input className="btn btn-info btn-lg w-100 mb-3" type="submit" />
-            </form>
-            
-        </div>
-    )
-}
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState("");
+
+  return (
+    <div className="container">
+      <form className="form-group">
+        <input
+          placeholder="Introduzca el nombre"
+          className="form-control mb-3"
+          type="text"
+          onChange ={(e) => {setNombre(e.target.value) 
+
+          }}
+        />
+        <input
+          placeholder="Introduzca la edad"
+          className="form-control  mb-3"
+          type="text"
+          onChange ={(e) => {setEdad(e.target.value) 
+
+          }}
+        />
+        <input className="btn btn-info btn-lg w-100 mb-3" type="submit" />
+      </form>
+    </div>
+  );
+};
